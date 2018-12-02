e98e4c2e97ae30fa1cf21426efdaa4d69c0b19c7
Jace Key <github.tamu.edu>
2018-12-02 15:11:31 -0600

    new

--- project2/errorHandling.js
+++
@@ -1,9 +0,0 @@
-
-window.onerror = function(msg, at, line, col, error) {
-   var extra = !col ? '' : '\ncolumn: ' + col;
-   extra += !error ? '' : '\nerror: ' + error;
-   alert("Error: " + msg + "\nat: " + at + "\nline: " + line + extra);
-   var suppressErrorAlert = true;
-   return suppressErrorAlert;
-};
-
